54bcba15a5519aa4cd2ab0c854490ec2a949ea47
Emran Billah <omegaregions0072004@yahoo.com>
2018-02-26 02:33:19 -0400

    All committed

--- controllers/home/homeTopController.js
+++ controllers/home/homeTopController.js
@@ -1,10 +1,45 @@
 (function () {
 
 app.controller("homeTopController", function (
-  $state, $stateParams, $scope
+  $state, $stateParams, $scope, $http
 ) {
 
-$scope.test = "test top";
+
+$scope.header = {};
+$scope.properties = [];
+$scope.property = {};
+var propertyId = '';
+
+if ($stateParams.id) {
+  var propertyId = $stateParams.id;
+}
+
+var getProperties = function () {
+  return $http.get("/static/data/properties.json")
+    .then(function (data) {
+      $scope.properties = data.data;
+    });
+};
+
+var getPropertyDetails = function (properties) {
+  $scope.property = {};
+  $scope.property = $scope.properties.find(function (row) {
+    return row.id == propertyId;
+  });
+
+  $scope.header = {
+    title: $scope.property.name,
+    description: $scope.property.neighbourhood
+  };
+
+  return $scope.property;
+};
+
+var loadPage = function () {
+  getProperties()
+    .then(getPropertyDetails);
+};
+loadPage();
 
 });
 
